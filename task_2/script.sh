f4445e80cd67898ab231ef8efac24d31f6f1abda
Anisov <dimaanisov24@gmail.com>
2020-08-14 00:50:11 +0300

    Change task2

--- task_2/script.sh
+++ task_2/script.sh
@@ -3,7 +3,20 @@
 mkdir -p ~root/.ssh
 cp ~vagrant/.ssh/auth* ~root/.ssh
 yum install -y mdadm smartmontools hdparm gdisk parted
-mdadm —create —verbose /dev/md10 —level=10 —raid-devices=4 /dev/sdb /dev/sdc /dev/sdd /dev/sde
+mdadm --create --verbose /dev/md10 --level=10 --raid-devices=4 /dev/sdb /dev/sdc /dev/sdd /dev/sde
+
+parted -s -a optimal -- /dev/md10 \
+    mklabel gpt \
+    mkpart primary 2048s 50MiB \
+    mkpart primary 50MiB 80MiB \
+    mkpart primary 80MiB 170MiB \
+    mkpart primary 170MiB 200MiB \
+    mkpart primary 200MiB -2048s
+mkdir -p /md_disks/part_{1,2,3,4,5}
+for i in $(seq 1 5); do mkfs.ext4 /dev/md10p$i; done
+for i in $(seq 1 5); do mount /dev/md10p$i /md_disks/part_$i; done
+for i in $(seq 1 5); do echo "/dev/md10p$i /md_disks/part_$i ext4 defaults 0 0" >> /etc/fstab; done
+
 parted -s -a optimal -- /dev/sdf \
     mklabel gpt \
     mkpart primary 2048s 50MiB \
